44301ca88208156a4974c20acb201831d66419aa
Kyle Raney <raney24fcb@gmail.com>
2016-02-10 20:37:16 -0500

    Initial Commit

---
+++ Quotes2.0/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Quotes2.0
+//
+//  Created by Kyle Raney on 2/10/16.
+//  Copyright © 2016 Kyle Raney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Quotes2.0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quotes2.0
+//
+//  Created by Kyle Raney on 2/10/16.
+//  Copyright © 2016 Kyle Raney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
